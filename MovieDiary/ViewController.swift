f838efe4fb08e5cee8bd7138178fcd92d1b39245
Can Yoldas <can.yoldas0@outlook.com>
2021-11-26 14:25:24 +0300

    Initial Commit

---
+++ MovieDiary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieDiary
+//
+//  Created by Can Yoldas on 26.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
